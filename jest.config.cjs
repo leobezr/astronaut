1bca0c0683db5ac3249410f8707ae1848b4c7a90
leobezr <le.bezr@gmail.com>
2022-01-21 22:32:35 -0300

    core: jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,5 +4,8 @@ module.exports = async () => {
   return {
     verbose: true,
     moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+    preset: "ts-jest",
+    testEnvironment: "jest-environment-node",
+    transform: {},
   };
 };
